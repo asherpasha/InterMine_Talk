f6be419727866a1363b7b545fdb1d5a9f7afd1bf
asherpasha <asher.pasha@utoronto.ca>
2020-01-28 19:47:15 -0500

    Minor bug

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 # Because I am too lazy!
 
 pdflatex Asher_ThaleMine.tex
-bibtex Asher_ThaleMine.aus
+bibtex Asher_ThaleMine.aux
 pdflatex Asher_ThaleMine.tex
 pdflatex Asher_ThaleMine.tex
 atril Asher_ThaleMine.pdf
